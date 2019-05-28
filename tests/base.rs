c68e504a7faef1c8c8dfcfc8187f4c28c19718cb
LudeeD <lfssilva@ua.pt>
2019-05-28 10:47:54 +0100

    update tests

--- tests/base.rs
+++ tests/base.rs
@@ -9,12 +9,13 @@ fn test_encode() {
         x: 112.5584f64,
         y: 37.8324f64,
     };
-    assert_eq!(encode(c0, 9usize).unwrap(), "ww8p1r4t8".to_string());
+    assert_eq!(encode(c0, 12usize).unwrap(), "e71150dc9947".to_string());
+
     let c1 = Coordinate {
         x: 117f64,
         y: 32f64,
     };
-    assert_eq!(encode(c1, 3usize).unwrap(), "wte".to_string());
+    assert_eq!(encode(c1, 3usize).unwrap(), "e65".to_string());
 
     let c2 = Coordinate {
         x: 190f64,
@@ -41,34 +42,34 @@ fn compare_decode(gh: &str, exp_lon: f64, exp_lat: f64, exp_lon_err: f64, exp_la
 
 #[test]
 fn test_decode() {
-    compare_decode("ww8p1r4t8", 112.558386, 37.832386, 0.000021457, 0.000021457);
-    compare_decode("9g3q", -99.31640625, 19.423828125, 0.17578125, 0.087890625);
+    compare_decode("e71150", 112.543945, 37.814941, 0.0439453125, 0.02197265625);
+    compare_decode("e65b4a", 117.02636 , 32.01416, 0.0439453125, 0.02197265625);
 
-    assert!(decode("abcd").is_err());
+    assert!(decode("wwgj").is_err());
 }
 
 #[test]
 fn test_neighbor() {
-    let ns = neighbors("ww8p1r4t8").unwrap();
-    assert_eq!(ns.sw, "ww8p1r4mr");
-    assert_eq!(ns.s, "ww8p1r4t2");
-    assert_eq!(ns.se, "ww8p1r4t3");
-    assert_eq!(ns.w, "ww8p1r4mx");
-    assert_eq!(ns.e, "ww8p1r4t9");
-    assert_eq!(ns.nw, "ww8p1r4mz");
-    assert_eq!(ns.n, "ww8p1r4tb");
-    assert_eq!(ns.ne, "ww8p1r4tc");
+    let ns = neighbors( "e71150dc99").unwrap();
+    assert_eq!(ns.sw,   "e71150dc92");
+    assert_eq!(ns.s,    "e71150dc98");
+    assert_eq!(ns.se,   "e71150dc9a");
+    assert_eq!(ns.w,    "e71150dc93");
+    assert_eq!(ns.e,    "e71150dc9b");
+    assert_eq!(ns.nw,   "e71150dc96");
+    assert_eq!(ns.n,    "e71150dc9c");
+    assert_eq!(ns.ne,   "e71150dc9e");
 }
 
 #[test]
 fn test_neighbor_wide() {
-    let ns = neighbors("9g3m").unwrap();
-    assert_eq!(ns.sw, "9g3h");
-    assert_eq!(ns.s, "9g3k");
-    assert_eq!(ns.se, "9g3s");
-    assert_eq!(ns.w, "9g3j");
-    assert_eq!(ns.e, "9g3t");
-    assert_eq!(ns.nw, "9g3n");
-    assert_eq!(ns.n, "9g3q");
-    assert_eq!(ns.ne, "9g3w");
+    let ns = neighbors("e7115").unwrap();
+    assert_eq!(ns.sw, "e5bbe");
+    assert_eq!(ns.s, "e7114");
+    assert_eq!(ns.se, "e7116");
+    assert_eq!(ns.w, "e5bbf");
+    assert_eq!(ns.e, "e7117");
+    assert_eq!(ns.nw, "e5bea");
+    assert_eq!(ns.n, "e7140");
+    assert_eq!(ns.ne, "e7142");
 }
